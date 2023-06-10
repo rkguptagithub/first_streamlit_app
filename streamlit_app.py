fe673055b400c9c0b5fe87123eddc187b4eb76e3
rkguptagithub <136060248+rkguptagithub@users.noreply.github.com>
2023-06-10 21:15:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ def get_fruit_load_list():
       return my_cur.fetchall()
 #Add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows=get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 streamlit.stop()
@@ -73,9 +73,17 @@ streamlit.stop()
 #my_data_rows = my_cur.fetchall()
 #streamlit.header("The Fruit Load List contains:")
 #streamlit.dataframe(my_data_rows)
-
+#Allow the end user to add afruit to the list
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values('from streamlit')")
+      return "Thanks for adding " + new_fruit
 #streamlit.header("What fruit would you like to add ?")
 add_my_fruit=streamlit.text_input('What fruit would you like to add ?')
+if streamlit.button('Add a Fruit to the list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   back_from_function=insert_row_snowflake(add_my_fruit)
+   streamlit.text(back-from_function)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute ("insert into fruit_load_list values('from streamlit')")
