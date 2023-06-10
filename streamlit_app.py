2d2d8db62c01be4f806914089b4d930df747f1cb
rkguptagithub <136060248+rkguptagithub@users.noreply.github.com>
2023-06-10 17:52:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ streamlit.dataframe(my_data_rows)
 
 #streamlit.header("What fruit would you like to add ?")
 add_my_fruit=streamlit.text_input('What fruit would you like to add ?')
-streamlit.write(fruit choice)
+streamlit.write('The user entered ',fruit choice)
 
